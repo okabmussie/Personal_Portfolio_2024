2cf90dbbf423033f1313fceb1c308954309810d5
okabmoss <s331690@oslomet.no>
2025-01-05 22:44:18 +0100

    Updated project structure and configurations

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,10 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.example.demo.DemoApplication.class) // Specify the main application class
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
